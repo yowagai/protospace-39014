91527abcdee9f0c4fa64ffa2f30a10fd361c0221
yowagai <imlf4btbw28@gmail.com>
2023-02-15 16:59:59 +0900

    first commit

---
+++ app/models/prototype.rb
@@ -0,0 +1,14 @@
+class Prototype < ApplicationRecord
+  has_many   :comments, dependent: :destroy
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :prototypes
+  has_many :comments
+
+  validates :name, presence: true
+end
+
